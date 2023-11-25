209d0d779496134324810aff38a649e484c84dc0
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-11-24 23:17:43 -0700

    Comportamiento del formField

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -7,28 +7,39 @@ class MessageFieldBox extends StatelessWidget {
   Widget build(BuildContext context) {
     //final colors = Theme.of(context).colorScheme;
 
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
           onPressed: () {
-            print('Valor caja de texto');
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
           },
           icon: const Icon(Icons.send_outlined)),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('Changed $value');
+        textController.clear();
+        // DESPUES DE QUE SE LIMPIA SIEMPRE VOY A QUERER EL FOCUS AHI
+        focusNode.requestFocus();
       },
     );
   }
