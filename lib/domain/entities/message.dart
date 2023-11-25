7a43fd0991d52bd19a3c81783be5ddaa3d8f3eaa
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-11-25 16:34:30 -0700

    Message Entity

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,10 @@
+enum FromWho { mine, hers }
+
+// ESTA ES LA ENTIDAD DE COMO LUCIRAN NUESTRO MENSAJES
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({required this.text, this.imageUrl, required this.fromWho});
+}
